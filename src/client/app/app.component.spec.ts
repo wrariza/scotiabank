bc57415c3399f8b5d1ccd8e3b4f367609e59ec02
William Ariza <william.ariza@almundo.com>
2019-05-19 09:55:42 -0500

    chore: implement angular material

--- src/client/app/app.component.spec.ts
+++ src/client/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed, async } from '@angular/core/testing'
 import { RouterTestingModule } from '@angular/router/testing'
 import { AppComponent } from './app.component'
+import { MatCardModule } from '@angular/material'
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, MatCardModule],
       declarations: [AppComponent],
     }).compileComponents()
   }))
@@ -27,7 +28,7 @@ describe('AppComponent', () => {
     fixture.detectChanges()
     const compiled = fixture.debugElement.nativeElement
     expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to scotiabank!'
+      'Welcome to scotiabank!',
     )
   })
 })
